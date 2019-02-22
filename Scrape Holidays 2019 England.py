1c276463ea64ec964d8212cd7db405a5e29181bd
my30 <myang30@outlook.com>
2019-02-22 14:55:08 +0000

    town-2019 town-2020

--- Scrape Holidays 2019 England.py
+++ Scrape Holidays 2019 England.py
@@ -15,8 +15,6 @@ town = 'barking-and-dagenham'
 holidays = pd.read_html('https://publicholidays.co.uk/school-holidays/england/barking-and-dagenham/')
 holidays_2019 = holidays[0]
 holidays_2020 = holidays[1]
-#print(holidays_2019, type(holidays_2019), holidays_2019.size)
-#print('\n', holidays_2019.iloc[:, 0])
 holidays_2019.insert(0, 'Local Authority', town)
 holidays_2020.insert(0, 'Local Authority', town)
 town_holidays = holidays_2019.append(holidays_2020, ignore_index=True)
