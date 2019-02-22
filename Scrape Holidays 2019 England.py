b96b6d8b68fc97701d121e9f786ea3394fd1361f
my30 <myang30@outlook.com>
2019-02-22 16:46:04 +0000

    Some LAs don't have 19-20 holidays ready yet. Scraping 2018-2019 only.

--- Scrape Holidays 2019 England.py
+++ Scrape Holidays 2019 England.py
@@ -10,7 +10,7 @@ for i in towns:
     LA = i[0]
     holidays = pd.read_html('https://publicholidays.co.uk/school-holidays/england/' + LA)
     holidays_2019 = holidays[0]
-    holidays_2020 = holidays[1]
+    #holidays_2020 = holidays[1]
     holidays_2019.insert(0, 'Local Authority', LA)
     #holidays_2020.insert(0, 'Local Authority', LA)
     #town_holidays = holidays_2019.append(holidays_2020, ignore_index=True)
