b2204e509cef60c2235dccd0ff85efe9b51f670e
my30 <myang30@outlook.com>
2019-02-25 11:20:25 +0000

    Final script for Scotland

--- Scrape Holiday 2019 Scotland.py
+++ Scrape Holiday 2019 Scotland.py
@@ -11,8 +11,8 @@ for i in towns:
     holidays = pd.read_html('https://publicholidays.co.uk/school-holidays/scotland/' + LA)
     holidays_2019 = holidays[0]
     holidays_2019.insert(0, 'Local Authority', LA)
-    holidays_2020 = holidays[1]
-    holidays_2020.insert(0, 'Local Authority', LA)
-    towns_holidays = holidays_2019.append(holidays_2020, ignore_index=False)
-    towns_holidays.to_csv('Scraped data/School Holidays 2018-2020 Scotland.csv', mode='a', header=True)
+    #holidays_2020 = holidays[1]
+    #holidays_2020.insert(0, 'Local Authority', LA)
+    #towns_holidays = holidays_2019.append(holidays_2020, ignore_index=False)
+    holidays_2019.to_csv('Scraped data/School Holidays 2018-2019 Scotland.csv', mode='a', header=True)
     print(str(LA) + ' is exported.')
