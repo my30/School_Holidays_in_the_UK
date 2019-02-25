dbd4b68d29478d2a7f3923256eb9bdb0fd6b7c16
Ming <myang30@outlook.com>
2019-02-25 10:40:46 +0000

    Directory added

--- Scrape Holidays 2019 Wales.py
+++ Scrape Holidays 2019 Wales.py
@@ -6,3 +6,12 @@ with open('List of Towns/townswales.csv', 'r') as fileTowns:
     towns = reader(fileTowns)
     towns = list(towns)
 
+for i in towns:
+    LA = i[0]
+    holidays = pd.read_html('https://publicholidays.co.uk/school-holidays/wales/' + LA)
+    holidays_2019 = holidays[0]
+    holidays_2019.insert(0, 'Local Authority', LA)
+    holidays_2020 = holidays[1]
+    holidays_2020.insert(0, 'Local Authority', LA)
+    towns_holidays = holidays_2019.append(holidays_2020, ignore_index = True)
+    towns_holidays.to_csv('')
