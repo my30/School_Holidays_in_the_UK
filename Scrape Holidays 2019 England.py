f9764d4161dec0d04ba8211dbc25d5b721ae187d
my30 <myang30@outlook.com>
2019-02-25 12:53:29 +0000

    Code optimisation

--- Scrape Holidays 2019 England.py
+++ Scrape Holidays 2019 England.py
@@ -2,6 +2,12 @@
 import pandas as pd
 from csv import reader
 
+filename = 'Scraped data/School Holidays 2018-2019 England.csv'
+# open and truncate the file
+f = open(filename, mode='w+')
+f.close()
+print('Data from' + filename + 'have been truncated. \nScraping now begins.')
+
 with open('List of Towns/townsengland.csv', 'r') as file_towns:
     towns = reader(file_towns)
     towns = list(towns)
@@ -15,5 +21,5 @@ for i in towns:
     #holidays_2020.insert(0, 'Local Authority', LA)
     #town_holidays = holidays_2019.append(holidays_2020, ignore_index=True)
     #town_holidays.to_csv('School Holidays 2018-2019 England.csv', mode='a', header=True)
-    holidays_2019.to_csv('School Holidays 2018-2019 England.csv', mode='a', header=True)
+    holidays_2019.to_csv('School Holidays 2018-2019 England.csv', mode='a', header=False)
     print(str(LA) + ' is exported.')
