be759418bc9a3055de8c6c39c6b9895dbe6f7642
my30 <myang30@outlook.com>
2019-02-25 11:32:59 +0000

    Final script for NI

--- Scrape Holidays 2019 Northern Ireland.py
+++ Scrape Holidays 2019 Northern Ireland.py
@@ -1,9 +1,8 @@
 # Ming 25/Feb/2019
 
 import pandas as pd
-from csv import reader
 
-holidays = pd.read_html('School Holidays 2018-2019 Northern Ireland.csv')
+holidays = pd.read_html('https://publicholidays.co.uk/school-holidays/northern-ireland/')
 holidays_2019 = holidays[0]
 holidays_2019.insert(0, 'Country', 'Northern Ireland')
 holidays_2019.to_csv('Scraped data/School Holidays 2018-2019 Northern Ireland.csv', mode='a', header=True)
