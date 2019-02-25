4d5dba0069d4d5bc9c46210d280e26232c346a7c
my30 <myang30@outlook.com>
2019-02-25 11:15:00 +0000

    Final script for Scotland

---
+++ Scrape Holiday 2019 Scotland.py
@@ -0,0 +1,18 @@
+#Ming 25/02/2019
+import pandas as pd
+from csv import reader
+
+with open('List of Towns/townsscotland.csv', 'r') as fileTowns:
+    towns = reader(fileTowns)
+    towns = list(towns)
+
+for i in towns:
+    LA = i[0]
+    holidays = pd.read_html('https://publicholidays.co.uk/school-holidays/scotland/' + LA)
+    holidays_2019 = holidays[0]
+    holidays_2019.insert(0, 'Local Authority', LA)
+    holidays_2020 = holidays[1]
+    holidays_2020.insert(0, 'Local Authority', LA)
+    towns_holidays = holidays_2019.append(holidays_2020, ignore_index=False)
+    towns_holidays.to_csv('Scraped data/School Holidays 2018-2020 Scotland.csv', mode='a', header=True)
+    print(str(LA) + ' is exported.')
